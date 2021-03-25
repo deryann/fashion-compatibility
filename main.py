3433ee5c560744d112e64f919eb426324def9de7
deryann <ryann@gmail.com>
2021-03-25 17:02:26 +0800

    Fix: In python 3 and pytorch 1.8 there is some error
    修正使其可以運作在 python3 與 prtorch 1.8
    Error:
    ```
    (base) D:\git_temp\fashion-compatibility-master>python main.py --test --l2_embed --resume runs/nondisjoint_l2norm/model_best.pth.tar
    C:\ProgramData\Anaconda3\lib\site-packages\torchvision\transforms\transforms.py:285: UserWarning: The use of the transforms.Scale transform is deprecated, please use transforms.Resize instead.
      warnings.warn("The use of the transforms.Scale transform is deprecated, " +
    => loading checkpoint 'runs/nondisjoint_l2norm/model_best.pth.tar'
    Traceback (most recent call last):
      File "main.py", line 312, in <module>
        main()
      File "main.py", line 138, in main
        checkpoint = torch.load(args.resume)
      File "C:\ProgramData\Anaconda3\lib\site-packages\torch\serialization.py", line 593, in load
        return _legacy_load(opened_file, map_location, pickle_module, **pickle_load_args)
      File "C:\ProgramData\Anaconda3\lib\site-packages\torch\serialization.py", line 772, in _legacy_load
        result = unpickler.load()
    UnicodeDecodeError: 'utf-8' codec can't decode byte 0xbe in position 2: invalid start byte
    ```
    
    ```
    (base) D:\git_temp\fashion-compatibility-master>python main.py --test --l2_embed --resume runs/nondisjoint_l2norm/model_best.pth.tar
    C:\ProgramData\Anaconda3\lib\site-packages\torchvision\transforms\transforms.py:285: UserWarning: The use of the transforms.Scale transform is deprecated, please use transforms.Resize instead.
      warnings.warn("The use of the transforms.Scale transform is deprecated, " +
    => loading checkpoint 'runs/nondisjoint_l2norm/model_best.pth.tar'
    => loaded checkpoint 'runs/nondisjoint_l2norm/model_best.pth.tar' (epoch 5)
    Traceback (most recent call last):
      File "main.py", line 312, in <module>
        main()
      File "main.py", line 149, in main
        test_acc = test(test_loader, tnet)
      File "main.py", line 244, in test
        embeddings.append(tnet.embeddingnet(images).data)
      File "C:\ProgramData\Anaconda3\lib\site-packages\torch\nn\modules\module.py", line 889, in _call_impl
        result = self.forward(*input, **kwargs)
      File "D:\git_temp\fashion-compatibility-master\type_specific_network.py", line 119, in forward
        masked_embedding = masked_embedding / norm.expand_as(masked_embedding)
    RuntimeError: The expanded size of the tensor (64) must match the existing size (66) at non-singleton dimension 2.  Target sizes: [256, 66, 64].  Tensor sizes: [256, 66]
    ```

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ def main():
     if args.resume:
         if os.path.isfile(args.resume):
             print("=> loading checkpoint '{}'".format(args.resume))
-            checkpoint = torch.load(args.resume)
+            checkpoint = torch.load(args.resume, encoding='latin1')
             args.start_epoch = checkpoint['epoch']
             best_acc = checkpoint['best_prec1']
             tnet.load_state_dict(checkpoint['state_dict'])
